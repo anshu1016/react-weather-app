d0ead5d825ca4a01dc51df0dd64edaa5f99cb509
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2024-02-25 10:09:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,8 +32,9 @@ const formatBackgroundColor = () =>{
  
   return (
     <>
-  <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 '>
-    <TopButtons setQ={setQ}/>
+  <div className='mx-auto max-w-screen-md mt-4 py-5 px-4 sm:px-0 md:px-16 lg:px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-auto sm:h-fit shadow-xl shadow-gray-400'>
+        <TopButtons setQ={setQ} />
+
     <Inputs weather={weather} setQ={setQ} setUnits={setUnits}/>
     {weather && (
       <div>
